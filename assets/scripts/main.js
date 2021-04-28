ce3d67bb4db2754120ed778bc40f5524a78fb2bd
Gustavo Carvalho <gsc13@tuta.io>
2021-04-28 14:06:41 -0300

    Update arrow function, Add openBookForm, Add even listeners

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,28 +1,41 @@
-let library = [];
-const addButton = document.querySelector('#addButton');
+const library = [];
+const addBookButton = document.querySelector('.addBook');
+const openFormButton = document.querySelector('.addNewBook');
+const bookForm = document.querySelector('.form');
 
 function Book(title, author, pages, read = false) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
-  this.info = function () {
-    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
-  }
+  this.info = () => `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
 }
 
-for (let i=0; i < 10; i++) {
-  let booook = new Book(`title${i}`, `author${i}`, i);
+for (let i = 0; i < 10; i += 1) {
+  const booook = new Book(`title${i}`, `author${i}`, i);
   library.push(booook);
 }
 
-function addBookToLibrary() {
-  const title = document.querySelector('#title').value;
-  const author = document.querySelector('#author').value;
-  const pages = document.querySelector('#pages').value;
+const addBookToLibrary = () => {
+  const title = document.querySelector('#title');
+  const author = document.querySelector('#author');
+  const pages = document.querySelector('#pages');
   const read = document.querySelector('#read');
 
-  const book = new Book(title, author, pages, read);
+  const book = new Book(title.value, author.value, pages.value, read);
   library.push(book);
-  return library;
-}
+
+  title.value = '';
+  author.value = '';
+  pages.value = '';
+
+  bookForm.classList.add('hide-form');
+};
+
+const openAddBookForm = () => {
+  bookForm.classList.remove('hide-form');
+};
+
+// event liteners
+openFormButton.addEventListener('click', openAddBookForm);
+addBookButton.addEventListener('click', addBookToLibrary);
